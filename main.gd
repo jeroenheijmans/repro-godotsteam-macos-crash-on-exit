494f58de4def9ed570e0157c64369f406611affc
Jeroen Heijmans <github@jeroenheijmans.nl>
2023-07-09 22:34:55 +0200

    Further simplify the repro after discussion on the related GitHub issue

--- main.gd
+++ main.gd
@@ -1,12 +1,6 @@
 extends Control
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	var result: Dictionary = Steam.steamInit(false)
-	print(result)
-
-
 func _on_button_button_down():
 	print("About to throw a test exception...")
 	push_error("Test Error")
